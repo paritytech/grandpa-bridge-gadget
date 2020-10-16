66f7a4118ad3dcc988b2eb3cee651680345bfdde
André Silva <andrerfosilva@gmail.com>
2020-10-16 01:30:43 +0100

    add skeleton for gossip processing

--- beefy-gadget/src/lib.rs
+++ beefy-gadget/src/lib.rs
@@ -1,14 +1,42 @@
 use std::collections::BTreeMap;
-use std::fmt::Debug;
 use std::sync::Arc;
 
 use futures::{FutureExt, Stream, StreamExt};
 use log::info;
+use parity_scale_codec::{Codec, Decode, Encode};
 
 use sc_client_api::{Backend as BackendT, BlockchainEvents, FinalityNotification, Finalizer};
-use sc_network_gossip::Network as GossipNetwork;
+use sc_network_gossip::{
+	GossipEngine, Network as GossipNetwork, ValidationResult as GossipValidationResult,
+	Validator as GossipValidator, ValidatorContext as GossipValidatorContext,
+};
 use sp_consensus::SyncOracle as SyncOracleT;
-use sp_runtime::traits::Block as BlockT;
+use sp_runtime::{
+	traits::{Block as BlockT, Hash as HashT, Header as HeaderT},
+	ConsensusEngineId,
+};
+
+pub const BEEFY_ENGINE_ID: ConsensusEngineId = *b"BEEF";
+pub const BEEFY_PROTOCOL_NAME: &'static str = "/paritytech/beefy/1";
+
+/// Allows all gossip messages to get through.
+struct AllowAll<Hash> {
+	topic: Hash,
+}
+
+impl<Block> GossipValidator<Block> for AllowAll<Block::Hash>
+where
+	Block: BlockT,
+{
+	fn validate(
+		&self,
+		_context: &mut dyn GossipValidatorContext<Block>,
+		_sender: &sc_network::PeerId,
+		_data: &[u8],
+	) -> GossipValidationResult<Block::Hash> {
+		GossipValidationResult::ProcessAndKeep(self.topic)
+	}
+}
 
 struct RoundTracker<Id, Signature> {
 	votes: Vec<(Id, Signature)>,
@@ -89,6 +117,7 @@ where
 struct BeefyWorker<Block: BlockT, Id, Signature, FinalityNotifications> {
 	rounds: Rounds<Block::Hash, Id, Signature>,
 	finality_notifications: FinalityNotifications,
+	gossip_engine: GossipEngine<Block>,
 }
 
 impl<Block, Id, Signature, FinalityNotifications>
@@ -96,20 +125,36 @@ impl<Block, Id, Signature, FinalityNotifications>
 where
 	Block: BlockT,
 {
-	fn new(voters: Vec<Id>, finality_notifications: FinalityNotifications) -> Self {
+	fn new(
+		voters: Vec<Id>,
+		finality_notifications: FinalityNotifications,
+		gossip_engine: GossipEngine<Block>,
+	) -> Self {
 		BeefyWorker {
 			rounds: Rounds::new(voters),
 			finality_notifications,
+			gossip_engine,
 		}
 	}
+}
+
+fn topic<Block: BlockT>() -> Block::Hash {
+	<<Block::Header as HeaderT>::Hashing as HashT>::hash("beefy".as_bytes())
+}
+
+#[derive(Decode, Encode)]
+struct VoteMessage<Hash, Id, Signature> {
+	block: Hash,
+	id: Id,
+	signature: Signature,
 }
 
 impl<Block, Id, Signature, FinalityNotifications>
 	BeefyWorker<Block, Id, Signature, FinalityNotifications>
 where
 	Block: BlockT,
-	Id: PartialEq,
-	Signature: PartialEq,
+	Id: Codec + PartialEq,
+	Signature: Codec + PartialEq,
 	FinalityNotifications: Stream<Item = FinalityNotification<Block>> + Unpin,
 {
 	fn handle_finality_notification(&mut self, notification: FinalityNotification<Block>) {
@@ -126,6 +171,10 @@ where
 	}
 
 	async fn run(mut self) {
+		let mut votes = Box::pin(self.gossip_engine.messages_for(topic::<Block>()).filter_map(|notification| async move {
+			VoteMessage::<Block::Hash, Id, Signature>::decode(&mut &notification.message[..]).ok()
+		}));
+
 		loop {
 			futures::select! {
 				notification = self.finality_notifications.next().fuse() => {
@@ -135,6 +184,13 @@ where
 						return;
 					}
 				},
+				vote = votes.next() => {
+					if let Some(vote) = vote {
+						self.handle_vote(vote.block, (vote.id, vote.signature));
+					} else {
+						return;
+					}
+				}
 			}
 		}
 	}
@@ -142,7 +198,7 @@ where
 
 pub async fn start_beefy_gadget<Block, Backend, Client, Network, SyncOracle>(
 	client: Arc<Client>,
-	_network: Network,
+	network: Network,
 	_sync_oracle: SyncOracle,
 ) where
 	Block: BlockT,
@@ -151,11 +207,23 @@ pub async fn start_beefy_gadget<Block, Backend, Client, Network, SyncOracle>(
 	Network: GossipNetwork<Block> + Clone + Send + 'static,
 	SyncOracle: SyncOracleT + Send + 'static,
 {
-	type Id = usize;
+	type Id = u64;
 	type Signature = ();
 
-	let mut worker =
-		BeefyWorker::<Block, Id, Signature, _>::new(vec![], client.finality_notification_stream());
+	let gossip_engine = GossipEngine::new(
+		network,
+		BEEFY_ENGINE_ID,
+		BEEFY_PROTOCOL_NAME,
+		Arc::new(AllowAll {
+			topic: topic::<Block>(),
+		}),
+	);
+
+	let worker = BeefyWorker::<_, Id, Signature, _>::new(
+		vec![],
+		client.finality_notification_stream(),
+		gossip_engine,
+	);
 
 	worker.run().await
 }
