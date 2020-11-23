184702a4bc38ab9538daef7833b93985b56afe2c
André Silva <andrerfosilva@gmail.com>
2020-11-23 16:54:51 +0000

    fix clippy error

--- node-example/node/src/service.rs
+++ node-example/node/src/service.rs
@@ -275,7 +275,7 @@ pub fn new_light(config: Configuration) -> Result<TaskManager, ServiceError> {
 	));
 
 	let (grandpa_block_import, _) =
-		sc_finality_grandpa::block_import(client.clone(), &(client.clone() as Arc<_>), select_chain.clone())?;
+		sc_finality_grandpa::block_import(client.clone(), &(client.clone() as Arc<_>), select_chain)?;
 
 	let aura_block_import =
 		sc_consensus_aura::AuraBlockImport::<_, _, _, AuraPair>::new(grandpa_block_import.clone(), client.clone());
