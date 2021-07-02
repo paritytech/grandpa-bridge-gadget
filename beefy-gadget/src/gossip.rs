db9934ca17f4600a189697011b2db8c83686a648
Tomasz Drwięga <tomasz@parity.io>
2021-07-02 14:31:42 +0200

    Implement known messages cache.

--- beefy-gadget/src/gossip.rs
+++ beefy-gadget/src/gossip.rs
@@ -17,10 +17,12 @@
 use codec::{Decode, Encode};
 use log::{debug, trace};
 use parking_lot::RwLock;
+use std::collections::BTreeMap;
 
 use sc_network::PeerId;
 use sc_network_gossip::{MessageIntent, ValidationResult, Validator, ValidatorContext};
 
+use sp_core::hashing::twox_64;
 use sp_runtime::traits::{Block, Hash, Header, NumberFor};
 
 use beefy_primitives::{
@@ -41,6 +43,11 @@ where
 	<<B::Header as Header>::Hashing as Hash>::hash(b"beefy")
 }
 
+/// A type that represents hash of the message.
+pub type MessageHash = [u8; 8];
+
+type KnownVotes<B> = BTreeMap<NumberFor<B>, fnv::FnvHashSet<MessageHash>>;
+
 /// BEEFY gossip validator
 ///
 /// Validate BEEFY gossip messages and limit the number of live BEEFY voting rounds.
@@ -54,7 +61,7 @@ where
 	B: Block,
 {
 	topic: B::Hash,
-	live_rounds: RwLock<Vec<NumberFor<B>>>,
+	known_votes: RwLock<KnownVotes<B>>,
 }
 
 impl<B> GossipValidator<B>
@@ -64,7 +71,7 @@ where
 	pub fn new() -> GossipValidator<B> {
 		GossipValidator {
 			topic: topic::<B>(),
-			live_rounds: RwLock::new(Vec::new()),
+			known_votes: RwLock::new(BTreeMap::new()),
 		}
 	}
 
@@ -77,19 +84,33 @@ where
 	pub(crate) fn note_round(&self, round: NumberFor<B>) {
 		trace!(target: "beefy", "🥩 About to note round #{}", round);
 
-		let mut live = self.live_rounds.write();
+		let mut live = self.known_votes.write();
 
-		if let Some(idx) = live.binary_search(&round).err() {
-			live.insert(idx, round);
+		if !live.contains_key(&round) {
+			live.insert(round, Default::default());
 		}
 
 		if live.len() > MAX_LIVE_GOSSIP_ROUNDS {
-			let _ = live.remove(0);
+			let to_remove = live.iter().next().map(|x| x.0).copied();
+			if let Some(first) = to_remove {
+				live.remove(&first);
+			}
 		}
 	}
 
-	fn is_live(live_rounds: &[NumberFor<B>], round: NumberFor<B>) -> bool {
-		live_rounds.binary_search(&round).is_ok()
+	fn add_known(known_votes: &mut KnownVotes<B>, round: &NumberFor<B>, hash: MessageHash) {
+		known_votes.get_mut(&round).map(|known| known.insert(hash));
+	}
+
+	fn is_live(known_votes: &KnownVotes<B>, round: &NumberFor<B>) -> bool {
+		known_votes.contains_key(round)
+	}
+
+	fn is_known(known_votes: &KnownVotes<B>, round: &NumberFor<B>, hash: &MessageHash) -> bool {
+		known_votes
+			.get(round)
+			.map(|known| known.contains(hash))
+			.unwrap_or(false)
 	}
 }
 
@@ -104,7 +125,24 @@ where
 		mut data: &[u8],
 	) -> ValidationResult<B::Hash> {
 		if let Ok(msg) = VoteMessage::<MmrRootHash, NumberFor<B>, Public, Signature>::decode(&mut data) {
+			let msg_hash = twox_64(&data);
+			let round = msg.commitment.block_number;
+
+			// Verify general usefulness of the message.
+			// We are going to discard old votes right away (without verification)
+			// Also we keep track of already received votes to avoid verifying duplicates.
+			{
+				let known_votes = self.known_votes.read();
+				if !GossipValidator::<B>::is_live(&known_votes, &round) {
+					return ValidationResult::Discard;
+				}
+				if GossipValidator::<B>::is_known(&known_votes, &round, &msg_hash) {
+					return ValidationResult::ProcessAndKeep(self.topic);
+				}
+			}
+
 			if BeefyKeystore::verify(&msg.id, &msg.signature, &msg.commitment.encode()) {
+				GossipValidator::<B>::add_known(&mut *self.known_votes.write(), &round, msg_hash);
 				return ValidationResult::ProcessAndKeep(self.topic);
 			} else {
 				// TODO: report peer
@@ -116,16 +154,17 @@ where
 	}
 
 	fn message_expired<'a>(&'a self) -> Box<dyn FnMut(B::Hash, &[u8]) -> bool + 'a> {
-		let live_rounds = self.live_rounds.read();
+		let known_votes = self.known_votes.read();
 		Box::new(move |_topic, mut data| {
 			let msg = match VoteMessage::<MmrRootHash, NumberFor<B>, Public, Signature>::decode(&mut data) {
 				Ok(vote) => vote,
 				Err(_) => return true,
 			};
 
-			let expired = !GossipValidator::<B>::is_live(&live_rounds, msg.commitment.block_number);
+			let round = msg.commitment.block_number;
+			let expired = !GossipValidator::<B>::is_live(&known_votes, &round);
 
-			trace!(target: "beefy", "🥩 Message for round #{} expired: {}", msg.commitment.block_number, expired);
+			trace!(target: "beefy", "🥩 Message for round #{} expired: {}", round, expired);
 
 			expired
 		})
@@ -133,16 +172,17 @@ where
 
 	#[allow(clippy::type_complexity)]
 	fn message_allowed<'a>(&'a self) -> Box<dyn FnMut(&PeerId, MessageIntent, &B::Hash, &[u8]) -> bool + 'a> {
-		let live_rounds = self.live_rounds.read();
+		let known_votes = self.known_votes.read();
 		Box::new(move |_who, _intent, _topic, mut data| {
 			let msg = match VoteMessage::<MmrRootHash, NumberFor<B>, Public, Signature>::decode(&mut data) {
 				Ok(vote) => vote,
 				Err(_) => return true,
 			};
 
-			let allowed = GossipValidator::<B>::is_live(&live_rounds, msg.commitment.block_number);
+			let round = msg.commitment.block_number;
+			let allowed = GossipValidator::<B>::is_live(&known_votes, &round);
 
-			trace!(target: "beefy", "🥩 Message for round #{} allowed: {}", msg.commitment.block_number, allowed);
+			trace!(target: "beefy", "🥩 Message for round #{} allowed: {}", round, allowed);
 
 			allowed
 		})
@@ -160,8 +200,8 @@ mod tests {
 
 		gv.note_round(1u64);
 
-		let live = gv.live_rounds.read();
-		assert!(GossipValidator::<Block>::is_live(&live, 1u64));
+		let live = gv.known_votes.read();
+		assert!(GossipValidator::<Block>::is_live(&live, &1u64));
 
 		drop(live);
 
@@ -169,14 +209,14 @@ mod tests {
 		gv.note_round(7u64);
 		gv.note_round(10u64);
 
-		let live = gv.live_rounds.read();
+		let live = gv.known_votes.read();
 
 		assert_eq!(live.len(), MAX_LIVE_GOSSIP_ROUNDS);
 
-		assert!(!GossipValidator::<Block>::is_live(&live, 1u64));
-		assert!(GossipValidator::<Block>::is_live(&live, 3u64));
-		assert!(GossipValidator::<Block>::is_live(&live, 7u64));
-		assert!(GossipValidator::<Block>::is_live(&live, 10u64));
+		assert!(!GossipValidator::<Block>::is_live(&live, &1u64));
+		assert!(GossipValidator::<Block>::is_live(&live, &3u64));
+		assert!(GossipValidator::<Block>::is_live(&live, &7u64));
+		assert!(GossipValidator::<Block>::is_live(&live, &10u64));
 	}
 
 	#[test]
@@ -188,12 +228,12 @@ mod tests {
 		gv.note_round(10u64);
 		gv.note_round(1u64);
 
-		let live = gv.live_rounds.read();
+		let live = gv.known_votes.read();
 
 		assert_eq!(live.len(), MAX_LIVE_GOSSIP_ROUNDS);
 
-		assert!(GossipValidator::<Block>::is_live(&live, 3u64));
-		assert!(!GossipValidator::<Block>::is_live(&live, 1u64));
+		assert!(GossipValidator::<Block>::is_live(&live, &3u64));
+		assert!(!GossipValidator::<Block>::is_live(&live, &1u64));
 
 		drop(live);
 
@@ -202,13 +242,13 @@ mod tests {
 		gv.note_round(20u64);
 		gv.note_round(2u64);
 
-		let live = gv.live_rounds.read();
+		let live = gv.known_votes.read();
 
 		assert_eq!(live.len(), MAX_LIVE_GOSSIP_ROUNDS);
 
-		assert!(GossipValidator::<Block>::is_live(&live, 15u64));
-		assert!(GossipValidator::<Block>::is_live(&live, 20u64));
-		assert!(GossipValidator::<Block>::is_live(&live, 23u64));
+		assert!(GossipValidator::<Block>::is_live(&live, &15u64));
+		assert!(GossipValidator::<Block>::is_live(&live, &20u64));
+		assert!(GossipValidator::<Block>::is_live(&live, &23u64));
 	}
 
 	#[test]
@@ -219,7 +259,7 @@ mod tests {
 		gv.note_round(7u64);
 		gv.note_round(10u64);
 
-		let live = gv.live_rounds.read();
+		let live = gv.known_votes.read();
 
 		assert_eq!(live.len(), MAX_LIVE_GOSSIP_ROUNDS);
 
@@ -228,12 +268,12 @@ mod tests {
 		// note round #7 again -> should not change anything
 		gv.note_round(7u64);
 
-		let live = gv.live_rounds.read();
+		let live = gv.known_votes.read();
 
 		assert_eq!(live.len(), MAX_LIVE_GOSSIP_ROUNDS);
 
-		assert!(GossipValidator::<Block>::is_live(&live, 3u64));
-		assert!(GossipValidator::<Block>::is_live(&live, 7u64));
-		assert!(GossipValidator::<Block>::is_live(&live, 10u64));
+		assert!(GossipValidator::<Block>::is_live(&live, &3u64));
+		assert!(GossipValidator::<Block>::is_live(&live, &7u64));
+		assert!(GossipValidator::<Block>::is_live(&live, &10u64));
 	}
 }
